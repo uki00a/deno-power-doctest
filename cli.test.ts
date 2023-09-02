975b81d9167ab1b3cffc1d9c4d58da71d370ca61
Yuki Tanaka <uki00a@gmail.com>
2023-09-02 22:45:32 +0900

    test(cli): add more tests (#5)

--- cli.test.ts
+++ cli.test.ts
@@ -1,11 +1,13 @@
 import { join } from "node:path";
-import { assertEquals } from "./deps.ts";
+import { assert, assertEquals } from "./deps.ts";
+import { assertStringIncludes } from "./test.deps.ts";
+
+const decoder = new TextDecoder();
+const baseDir = new URL("./", import.meta.url).pathname;
 
 Deno.test({
-  name: "CLI",
+  name: "CLI - testdata/cli.md",
   fn: async () => {
-    const decoder = new TextDecoder();
-    const baseDir = new URL("./", import.meta.url).pathname;
     const output = await new Deno.Command("deno", {
       args: [
         "run",
@@ -18,16 +20,46 @@ Deno.test({
       env: { NO_COLOR: "1" },
     }).output();
     const stderr = decoder.decode(output.stderr);
-    assertEquals(output.success, true, stderr);
+    assert(output.success, stderr);
 
     const stdout = decoder.decode(output.stdout).trim();
     const expected =
-      (await Deno.readTextFile(join(baseDir, "testdata/cli.output.txt")))
+      (await Deno.readTextFile(join(baseDir, "testdata/cli.stdout.txt")))
         .trim();
     assertEquals(stdout, expected);
   },
   permissions: {
-    read: ["testdata/cli.output.txt"],
+    read: ["testdata/cli.stdout.txt"],
+    run: ["deno"],
+  },
+});
+
+Deno.test({
+  name: "CLI - testdata/fail.md",
+  fn: async () => {
+    const output = await new Deno.Command("deno", {
+      args: [
+        "run",
+        "--no-prompt",
+        "--allow-read=testdata/fail.md",
+        "cli.ts",
+        "testdata/fail.md",
+      ],
+      cwd: baseDir,
+      env: { NO_COLOR: "1" },
+    }).output();
+    const stdout = decoder.decode(output.stdout).trim();
+    const stderr = decoder.decode(output.stderr).trim();
+    assert(!output.success, stderr);
+
+    const expected =
+      (await Deno.readTextFile(join(baseDir, "testdata/fail.stdout.txt")))
+        .trim();
+    assertEquals(stdout, expected);
+    assertStringIncludes(stderr, "Error: Tests failed");
+  },
+  permissions: {
+    read: ["testdata/fail.stdout.txt"],
     run: ["deno"],
   },
 });
---
+++ test.deps.ts
@@ -0,0 +1,3 @@
+export {
+  assertStringIncludes,
+} from "https://deno.land/std@0.200.0/assert/assert_string_includes.ts";
