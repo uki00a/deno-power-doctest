d4296df12cb5037cd5147eac46765165d9ffbc80
uki00a <uki00a@gmail.com>
2023-10-19 19:24:39 +0900

    feat: support more styles of comments

--- typescript.ts
+++ typescript.ts
@@ -1,45 +1,66 @@
 import { createProject, withSourceFile } from "./internal/ts-morph/mod.ts";
 import type { CodeBlock } from "./_code_block.ts";
 import { parse as parseMarkdown } from "./markdown.ts";
-import { Node } from "./deps.ts";
+import type {
+  JSDocLink,
+  JSDocLinkCode,
+  JSDocLinkPlain,
+  JSDocText,
+} from "./deps.ts";
+import { Node, ts } from "./deps.ts";
 
 export function parse(content: string): Array<CodeBlock> {
   const project = createProject();
   return withSourceFile(
     (sourceFile) => {
-      return sourceFile.getStatementsWithComments()
-        .flatMap((stmt) => {
-          if (!Node.isJSDocable(stmt)) return [];
+      const foundBlocks: Array<CodeBlock> = [];
+      sourceFile.forEachDescendant((node) => {
+        if (!Node.isJSDocable(node)) return;
 
-          return stmt.getJsDocs().flatMap((x) => {
-            const { comment } = x.compilerNode;
-            if (comment == null) return [];
+        for (const x of node.getJsDocs()) {
+          const comment = x.getComment();
+          if (comment == null) continue;
 
-            const origStartLine = x.getStartLineNumber();
-            if (typeof comment === "string") {
-              return parseMarkdown(comment).map(({ range, ...x }) => {
-                return {
-                  ...x,
-                  range: {
-                    start: {
-                      ...range.start,
-                      line: origStartLine + range.start.line,
-                    },
-                    end: {
-                      ...range.end,
-                      line: origStartLine + range.end.line,
-                    },
-                  },
-                };
-              });
-            } else {throw new Error(
-                "`ts.NodeArray<ts.JSDocComment>` is not supported yet.",
-              );}
+          const origStartLine = x.getStartLineNumber();
+          const body = typeof comment === "string"
+            ? comment
+            : comment.map(getJSDocBody).join("");
+          const blocks = parseMarkdown(body).map(({ range, ...x }) => {
+            return {
+              ...x,
+              range: {
+                start: {
+                  ...range.start,
+                  line: origStartLine + range.start.line,
+                },
+                end: {
+                  ...range.end,
+                  line: origStartLine + range.end.line,
+                },
+              },
+            };
           });
-        });
+          foundBlocks.push(...blocks);
+        }
+      });
+      return foundBlocks;
     },
     project,
     "tmp.ts",
     content,
   );
+}
+
+function getJSDocBody(
+  jsdoc: JSDocText | JSDocLink | JSDocLinkCode | JSDocLinkPlain | undefined,
+) {
+  if (jsdoc == null) {
+    return "";
+  }
+  if (jsdoc.isKind(ts.SyntaxKind.JSDocText)) {
+    // NOTE: `JSDocText#getText()` returns a comment with preceding `/*` or `*`.
+    return jsdoc.compilerNode.text;
+  } else {
+    return jsdoc.getText();
+  }
 }
